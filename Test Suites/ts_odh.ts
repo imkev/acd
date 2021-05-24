55ef771199e8e8602ffa18f4443899edd2c9282a
Luo <weiyu.luo@accenture.com>
2021-05-23 22:56:43 -0400

    first commit

---
+++ Test Suites/ts_odh.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts_odh</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4fb34bf4-ccfe-484e-89cb-6002d3e2f7b3</testSuiteGuid>
+   <testCaseLink>
+      <guid>32d08287-697d-4484-8e65-872bf6358ee0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/operations/GoogleSearch</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
